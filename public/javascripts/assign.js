56dd84e26b2d202c0957968320c7faa0efb8518f
Borja Domínguez <borja.d.atienza@gmail.com>
2020-02-26 21:29:38 +0100

    assign page 90%

--- public/javascripts/assign.js
+++ public/javascripts/assign.js
@@ -4,27 +4,54 @@ window.assignItems = axios.create({
 
 let e;
 
+addEventListener("load", noDefault());
+
 // Refresh Currents Items
 
 function refresh() {
+  noDefault();
   e = document.getElementById("refresh-item").name;
   console.log(e);
-  axios
-    .get(`http://localhost:3000/assign/${e}/assign`, OK)
-    .then(response => console.log("Ahí va"));
+  axios.get(`/assign/${e}/refresh`).then(response => {
+    document.getElementById("table-assign-1").style.display = "none";
+    document.getElementById("table-assign-2").style.display = "block";
+    createCells(response);
+  });
 }
 
-// When the WALL-E button is clicked
+// No default function of button
+function noDefault() {
+  document
+    .getElementById("assign-item")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+    });
+}
+
+// Assign Item
 function assign() {
   e = document.getElementById("refresh-item").name;
   const item = event.target.name;
   console.log(item);
   axios
-    .post(`http://localhost:3000/assign/${e}/assign`, { item, e })
+    .post(`/assign/newItem`, { item, e })
     .then(refresh())
     .catch(error => console.log("Oh No! Error is: ", error));
 }
 
 function id() {
   e = document.getElementById("refresh-item").name;
+}
+
+function createCells(response) {
+  console.log(response.data.employee.equipmentsId);
+  let data = response.data.employee.equipmentsId;
+  let table = document.getElementsByClassName("refresh-cells")[0];
+  let tr = document.createElement("tr");
+  let td = document.createElement("td");
+  let newContent = "";
+  for (let i = 0; i < data.length; i++) {
+    newContent += `<tr><td>${data[i]}</td></tr>`;
+  }
+  table.innerHTML = newContent;
 }
--- routes/assignRouter.js
+++ routes/assignRouter.js
@@ -21,14 +21,47 @@ router.get("/:id/assign", async (req, res, next) => {
   }
 });
 
-router.post("/:id/assign", async (req, res, next) => {
+// Refresh
+
+router.get("/:id/refresh", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employees.findById(id);
+    const equipment = await Equipments.find();
+    console.log("!!!!!");
+    console.log(employee);
+    res.json({
+      employee,
+      equipment
+    });
+  } catch (error) {
+    console.log("Employee dont found");
+  }
+});
+
+// router.get("/:id/assign", async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const employee = await Employees.findById(id);
+//     const equipment = await Equipments.find();
+//     console.log("Recibido!!!!!");
+//     res.render("assignEquipments/assignItem", {
+//       employee,
+//       equipment
+//     });
+//   } catch (error) {
+//     console.log("Employee dont found");
+//   }
+// });
+
+router.post("/newItem", async (req, res, next) => {
   console.log(req.body);
   const { item, e } = req.body;
   console.log(item, e, "JA!");
   const id = e;
   const reference = item;
   // const employee = await Employees.findById(id);
-  const newAssign = await Employees.updateOne(
+  await Employees.updateOne(
     {
       _id: id
     },
@@ -38,13 +71,15 @@ router.post("/:id/assign", async (req, res, next) => {
       }
     }
   );
-  // try {
-  //   console.log(`${newAssign} assigned `);
-  //   return res.redirect(`/assign/${id}/assign`);
-  // } catch {
-  //   console.log(`${newAssign} assigned but error`);
-  //   next();
-  // }
+  res.json({});
 });
+
+// try {
+//   console.log(`${newAssign} assigned `);
+//   return res.redirect(`/assign/${id}/assign`);
+// } catch {
+//   console.log(`${newAssign} assigned but error`);
+//   next();
+// }
 
 module.exports = router;
