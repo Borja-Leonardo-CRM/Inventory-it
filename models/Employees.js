f9c9d229759c6d97d471c12694160f8f7f882f28
Borja Domínguez <borja.d.atienza@gmail.com>
2020-02-29 20:54:18 +0100

    40% CSS DONE

--- models/Employees.js
+++ models/Employees.js
@@ -10,3 +10,10 @@ const schemaEmployees = new mongoose.Schema({
 const model = mongoose.model("Employees", schemaEmployees);
 
 module.exports = model;
+
+// const schemaEmployees = new mongoose.Schema({
+//   identity: Number,
+//   name: String,
+//   department: String,
+//   equipmentsId: [{ type: Schema.ObjectId, ref: "Equipments" }]
+// });
